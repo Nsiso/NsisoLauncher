02637782d796c5bc990132d18e5abef0acf4e209
张珉玮 <nsisogf@163.com>
2019-11-13 22:48:57 +0800

    setup unit test and ready to update mvvm

---
+++ src/NsisoLauncher.Test/LaunchTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NsisoLauncherCore;
+using NsisoLauncherCore.Util;
+
+namespace NsisoLauncher.Test
+{
+    [TestClass]
+    public class LaunchTest
+    {
+        LaunchHandler launchHandler;
+
+        [TestMethod]
+        public async void TestLaunching()
+        {
+            //launchHandler = new LaunchHandler(".minecraft/", Java.GetSuitableJava(), true);
+            //await launchHandler.LaunchAsync(new NsisoLauncherCore.Modules.LaunchSetting()
+            //{
+                
+            //});
+        }
+    }
+}
---
+++ src/NsisoLauncher.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NsisoLauncher.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NsisoLauncher.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99af489e-fd2d-4e64-b470-70ada4e3e591")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
