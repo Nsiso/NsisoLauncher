fb5b59df3635e8c848829fed7ccb3ed73dd4ae68
Siso <nsisogf@163.com>
2020-12-29 19:42:31 +0800

    Test ci

--- src/NsisoLauncher/Views/Windows/MainWindow.xaml.cs
+++ src/NsisoLauncher/Views/Windows/MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace NsisoLauncher.Views.Windows
             InitializeComponent();
 
             //THIS IS NOT MVVM
-            //Hello world!
             vm.NavigationService = frame.NavigationService;
             vm.InitializeMainPage();
             App.LogHandler.AppendDebug("启动器主窗体已载入");
